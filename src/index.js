7d84ffc60b1433c4776e8c646505baee92849cce
Michael Venegas <lim_venegas@hotmail.com>
2021-10-22 16:28:29 -0500

    Component handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h4>Warning</h4>
+                <div>Are you sure want delete?</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Pepe' 
+                    timeAgo='Today at 6:00PM' 
+                    comment='Good post' 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Miguelon' 
+                    timeAgo='Today at 02:00PM' 
+                    comment='other post' 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Micky' 
+                    timeAgo='Yesterday at 05:00PM' 
+                    comment='Good job'
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<App/>, document.getElementById('root'));
